90f05fe5e9c46207ed3a9d936440700503e49903
Eduardo <webdu15@gmail.com>
2021-02-18 00:51:24 -0300

    Javascript aula 2

---
+++ js/functions.js
@@ -0,0 +1,82 @@
+window.onload = function(){
+
+    // fruta = {nome:"maça", cor:"vermelha"}
+    // console.log(fruta.nome)
+    // alert(fruta.cor)
+
+    // fruta = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
+    // alert(fruta[1].nome)
+
+    // lista = ["maça", "pera", "laranja"]
+    // lista.push("uva")
+    // lista.pop()
+
+    // console.log(lista.toString())
+    // console.log(lista.join("-"))
+    // console.log(lista.reverse())
+    // console.log(lista.length)
+    // nome = prompt('Qual é o seu nome?')
+    // ano = prompt('Em que ano você nasceu?')
+    // idade = 2021-ano
+    // alert(`Muito prazer ${nome}, você tem ${idade} anos`)
+
+    // idade = 18
+    // if (idade >= 18){
+    //     alert('Maior de idade!')
+    // } else {
+    //     alert ('Menor de idade')
+    // }
+
+    // idade = prompt("Qual sua idade")
+    // if (idade >= 18){
+    //     alert('Maior de idade!')
+    // } else {
+    //     alert ('Menor de idade')
+    // }
+
+    // count = 0
+    
+    // while (count <= 5){
+    //     console.log(count)
+    //     count++
+    // }
+
+    // var count 
+    // for (count = 0; count <= 5; count++){
+    //     alert(count)
+    // }
+
+    // d = new Date()
+    // alert(d.getMonth()+1)
+    // alert(d.getHours())
+    // alert(d.getMinutes())
+
+    // function soma(n1,n2){
+    //     return n1+n2
+    // }
+
+    // alert(soma(5, 3))
+
+    // function setReplace(frase, nome, novo_nome){
+    //     return frase.replace(nome,novo_nome)
+    // }
+
+    // alert(setReplace("Vai Japão", "Japão", "Brasil"))
+    
+    // var validar;
+
+    // function validaIdade(idade){
+    //     if(idade >= 18){
+    //         validar = true
+    //     } else {
+    //         validar = false 
+    //     }
+
+    //     return validar
+    // }
+
+    // idade = prompt("Qual sua idade?")
+    // console.log(validaIdade(idade))
+
+
+}
